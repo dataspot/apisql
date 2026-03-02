615dd2d6715c382bb2c5830f09a434147ff1090e
Adam Kariv <adam.kariv@gmail.com>
2026-03-02 20:46:17 +0100

    More wide bot detection

--- apisql/blueprint.py
+++ apisql/blueprint.py
@@ -159,7 +159,7 @@ class APISQLBlueprint(Blueprint):
     def detect_bot(self):
         if request.user_agent.browser in ('google', 'aol', 'baidu', 'bing', 'yahoo'):
             logger.info('Bot detected %s: %s', request.user_agent.string, request.user_agent.browser)
-        elif any(x in request.user_agent.string.lower() for x in ('applebot', 'yandexbot', 'petalbot')):
+        elif any(x in request.user_agent.string.lower() for x in ('bot', 'crawl', 'spider', 'slurp')):
             logger.info('Bot detected %s: %s', request.user_agent.string, request.user_agent.browser)
         else:
             return False
